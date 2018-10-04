ba7f5cacfc7051169e87650c21a17646e37d7a75
eryshkov <wendor265>
2018-10-04 22:45:10 +0300

    init commit

--- Scroll View/ViewController.swift
+++ Scroll View/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        registerForNotifications()
+
     }
 
+    func registerForNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWasShawn(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWasShawn(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+    }
 
+    @objc func keyBoardWasShawn(_ notification: NSNotification) {
+        let heightDifference: CGFloat = 3
+        
+        guard let info = notification.userInfo, let keyboardFrameValue = info[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue else { return }
+        
+        let keyboardFrame = keyboardFrameValue.cgRectValue
+        let keyboardSize = keyboardFrame.size
+        
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height + heightDifference, right: 0)
+        
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyBoardWillBeHidden(_ notification: NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
 }
 
